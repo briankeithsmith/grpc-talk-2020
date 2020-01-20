758a859aaf2bf1ac3218a1229bdaf80dff9b1be1
abvaden <6742744+abvaden@users.noreply.github.com>
2020-01-20 16:03:13 +0000

    Deploying to gh-pages from  - dbd4b9e0bff882428e75661f0cd2ae92f3af9688 🚀

---
+++ precache-manifest.be78d7f4fceee0dafd2870c9f2509277.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb989cd22b881ca495fd",
+    "url": "/grpc-talk-2020/css/app.2e68432d.css"
+  },
+  {
+    "revision": "04fd2baf9113a37effea",
+    "url": "/grpc-talk-2020/css/slides.1031ef28.css"
+  },
+  {
+    "revision": "b8eb88b8f35494bb5b131b124aae8253",
+    "url": "/grpc-talk-2020/favicon.png"
+  },
+  {
+    "revision": "949404dc3ace6753b7f94179cf301b41",
+    "url": "/grpc-talk-2020/img/grpc-logo.949404dc.png"
+  },
+  {
+    "revision": "f1a4a6b2c34b9abc49a2bfe33e66056f",
+    "url": "/grpc-talk-2020/index.html"
+  },
+  {
+    "revision": "cb989cd22b881ca495fd",
+    "url": "/grpc-talk-2020/js/app.e0dbdf15.js"
+  },
+  {
+    "revision": "ab28f0a8b93032e272c1",
+    "url": "/grpc-talk-2020/js/chunk-vendors.2f9f58a2.js"
+  },
+  {
+    "revision": "5427c359b1e1d8c73068",
+    "url": "/grpc-talk-2020/js/slides-section-1.6ae0af6d.js"
+  },
+  {
+    "revision": "c6b6557ec910ecc97165",
+    "url": "/grpc-talk-2020/js/slides-section-2.5e13d484.js"
+  },
+  {
+    "revision": "3449aa33c1cb612b9289",
+    "url": "/grpc-talk-2020/js/slides-section-3.780dc0dd.js"
+  },
+  {
+    "revision": "04fd2baf9113a37effea",
+    "url": "/grpc-talk-2020/js/slides.f3e96e36.js"
+  },
+  {
+    "revision": "9494858f49622ce736e5c50753e5b042",
+    "url": "/grpc-talk-2020/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/grpc-talk-2020/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/grpc-talk-2020/precache-manifest.be78d7f4fceee0dafd2870c9f2509277.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "presentation-client"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
